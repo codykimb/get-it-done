99662cb4f35753c9ca45ba6f647cdf3315db4499
Cody Kimb <codykimbell@gmail.com>
2021-01-12 12:44:32 -0600

    set up js file

---
+++ assets/js/homepage.js
@@ -0,0 +1,9 @@
+var getUserRepos = function() {
+    
+    var getUserRepos = function() {
+        fetch("https://api.github.com/users/octocat/repos");
+      };
+      
+  };
+  
+  getUserRepos();
