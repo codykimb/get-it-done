06c0d7e6170647c842ca4c15066b1c6272c01a5c
Cody Kimb <codykimbell@gmail.com>
2021-01-12 20:16:41 -0600

    see list of open issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,23 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
 
 var getRepoIssues = function(repo) {
 
@@ -10,17 +29,20 @@ var getRepoIssues = function(repo) {
           response.json().then(function(data) {
             // pass response data to dom function
             displayIssues(data);
+
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
           });
         }
         else {
-          alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
       });
 
 };
   
-getRepoIssues("codykimb/taskinator");
-
 var displayIssues = function(issues) {
 
     if (issues.length === 0) {
@@ -60,3 +82,19 @@ var displayIssues = function(issues) {
       }
 
 };
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+
+};
+
+getRepoName();
